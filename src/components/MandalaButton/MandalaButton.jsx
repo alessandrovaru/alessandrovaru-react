0ae7cbce21ef65a53d5fe2e0c4a2bbbe8842ef2a
Alessandrovaru <alessandrovaru@gmail.com>
2023-01-05 23:29:32 -0400

    Refactoring and fixing errors

--- src/components/MandalaButton/MandalaButton.jsx
+++ src/components/MandalaButton/MandalaButton.jsx
@@ -1,8 +1,9 @@
 import VanillaTilt from 'vanilla-tilt';
 import { useEffect, useRef, useState } from 'react'
 import logo from '../../logomandala.png';
-import mand from '../../images/mand1.jpg';
+
 import styled from "styled-components";
+import Poem from '../Poem/Poem';
 
 const MandalaContainer = styled.div`
   animation: slideInFromLeft 2s ease-in-out;
@@ -30,36 +31,7 @@ const MandalaPoemButton = styled.button`
   }
 `
 
-const PoemContainer = styled.div`
-  animation: slideInFromLeft 2s ease-in-out;
-  p{
-    font-family:'Questrial';
-    font-size: 16px;
-  }
-  h1{
-    font-size: 12px
-  }
-  a{
-    text-decoration: none;
-    color: white
-  }
-  a:hover{
-    text-decoration: none;
-    color: gray
-  }
-  @media only screen and (max-width: 900px) {
-    p{
-      font-family:'Questrial';
-      font-size: 10px;
-    }
-    h1{
-      font-size: 8px
-    }
-    h2{
-      font-size: 18px
-    }
-  }
-`
+
 
 const MandalaButton = () => {
   const [mandalaClicked, setMandalaClicked] = useState(false)
@@ -123,19 +95,7 @@ const MandalaButton = () => {
           </>
         }
         {poemClicked && 
-          <PoemContainer>
-            <img onClick={randomSong} src={mand} className="App-logo" alt="logo" />
-            <h2>Nausea Ansiosa</h2>
-            <p>Estuvo aquí dentro de mí en todo momento.</p>
-            <p>Un nudo en la garganta, que no puedo evitar,</p>
-            <p>que me deja sin aire, sin salir mi lamento;</p>
-            <p>y se siente como una nausea ansiosa</p>
-            <p>que siempre me acompaña al despertar.</p>  
-            <br></br>
-            <p>La intento soltar, pero vuelve dentro de mí.</p>
-            <br></br>
-            <a href="https://instagram.com/alessandrovaru" target="_blank" rel="noreferrer"><h1>ALESSANDROVARU</h1></a>
-          </PoemContainer>
+          <Poem randomLink={randomLink}/>
         }
       </Tilt>
     </MandalaContainer>
